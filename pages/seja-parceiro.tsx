df3df067601b2b335eb99bfb2b8474f0a1c06930
wesleybridi <wesleybridi@gmail.com>
2022-07-28 21:09:59 -0300

    atualizacao

--- pages/seja-parceiro.tsx
+++ pages/seja-parceiro.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link'
 import Header from '../components/header'
 
-export default function Contato() {
+export default function Sejaparceiro() {
   return (
     <>
       <Header />
