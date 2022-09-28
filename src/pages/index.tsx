299efe1ec60645b80a9834ed5f9193c17e727576
Gabriel Moresco <gabriel.moresco25@gmail.com>
2022-09-28 19:30:09 -0300

    alterado os arquivos das Sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ import Head from 'next/head'
 import Header from '../components/Header'
 
 // SECTIONS
-import InicioSection from '../sections/Inicio-section'
-import ComoEncontrarSection from '../sections/ComoEncontrar-section'
-import ClientesSection from '../sections/Clientes-section'
-import DepoimentosSection from '../sections/Depoimentos-section'
-import ApoiadoresSection from '../sections/Apoiadores-section'
+import InicioSection from '../sections/InicioSection'
+import ComoEncontrarSection from '../sections/ComoEncontrarSection'
+import ClientesSection from '../sections/ClientesSection'
+import DepoimentosSection from '../sections/DepoimentosSection'
+import ApoiadoresSection from '../sections/ApoiadoresSection'
 import Invite from '../components/Invite'
 import Footer from '../components/Footer'
 
