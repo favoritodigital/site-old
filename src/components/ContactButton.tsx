002fd37ce33ca330724e0e2669f2eced4055b759
wesleybridi <wesleybridi@gmail.com>
2022-09-29 00:10:31 -0300

    Correção no botão de contatos

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -2,7 +2,12 @@ import WhatsappIcon from '/public/images/whatsapp-icon-mobile.svg'
 
 export default function ContactButton() {
   return (
-    <a href='https://wa.me/5547997536595' target='_blank' className='whatsappbutton'>
+    <a
+      href='https://wa.me/5547997536595'
+      target='_blank'
+      className='whatsappbutton'
+      rel='noreferrer'
+    >
       <WhatsappIcon />
     </a>
   )
