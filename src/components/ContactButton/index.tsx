b30affd1c0faedc235ded5047237d400dbf10c47
wesleybridi <wesleybridi@gmail.com>
2022-10-10 09:19:58 -0300

    Separada a estilização do ContactButton

--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -1,11 +1,12 @@
 import WhatsappIcon from '/public/images/whatsapp-icon-mobile.svg'
+import styles from './ContactButton.module.css'
 
 export default function ContactButton() {
   return (
     <a
       href='https://wa.me/5547997536595'
       target='_blank'
-      className='whatsappbutton'
+      className={styles.button}
       rel='noreferrer'
     >
       <WhatsappIcon />
