75e72f7963af7d71e89d48f60bb0070362e5c8d1
wesleybridi <wesleybridi@gmail.com>
2022-10-13 16:42:50 -0300

    Separada a estilização do InviteSection

--- src/sections/InviteSection/index.tsx
+++ src/sections/InviteSection/index.tsx
@@ -1,6 +1,8 @@
+import styles from './styles.module.css'
+
 export default function InviteSection() {
   return (
-    <div id='invite'>
+    <div id='invite' className={styles.container}>
       <p>e aí barbadinha, bora pro rolê? ;)</p>
     </div>
   )
