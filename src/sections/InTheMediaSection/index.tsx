e2f1fdd157e5ee5db7ed595e2a46ab754903bfc3
wesleybridi <wesleybridi@gmail.com>
2022-11-08 22:19:09 -0300

    Alteração dos ID's da section

--- src/sections/InTheMediaSection/index.tsx
+++ src/sections/InTheMediaSection/index.tsx
@@ -3,8 +3,8 @@ import SlidesInTheMedia from '../../components/SlidesInTheMedia'
 
 export default function InTheMediaSection() {
   return (
-    <div className={styles.container} id='que-barbada-na-midia-section'>
-      <div className={styles.pin} id='que-barbada-na-midia'></div>
+    <div className={styles.container} id='na-midia-section'>
+      <div className={styles.pin} id='na-midia'></div>
 
       <h1 className={styles.sectionTitle}>Que Barbada na mídia</h1>
       <SlidesInTheMedia />
