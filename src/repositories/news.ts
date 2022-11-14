275e04932b95a10604777dff9750226f9680012d
wesleybridi <wesleybridi@gmail.com>
2022-11-14 19:33:26 -0300

    Compactado imagens dos cards

--- src/repositories/news.ts
+++ src/repositories/news.ts
@@ -3,7 +3,7 @@ export const news = [
     name: 'Engeplus',
     url: 'https://www.engeplus.com.br/noticia/espaco-empreendedor/2022/aplicativo-promete-facilitar-happy-hour-em-criciuma-e-regiao',
     title: 'Aplicativo promete facilitar Happy Hour em Criciúma e Região',
-    imgFilePath: '/images/quebarbada-engeplus-noticenter.jpg',
+    imgFilePath: '/images/quebarbada-engeplus-noticenter.png',
   },
   {
     name: 'CanalSC',
@@ -15,6 +15,6 @@ export const news = [
     name: 'Noticenter',
     url: 'https://www.noticenter.com.br/n.php?ID=31809&T=aplicativo-que-barbada-criado-em-jaragua-do-sul-chega-para-descomplicar-o-role',
     title: "Aplicativo Que Barbada, criado em Jaraguá do Sul, chega para 'descomplicar o rolê'",
-    imgFilePath: '/images/quebarbada-engeplus-noticenter.jpg',
+    imgFilePath: '/images/quebarbada-engeplus-noticenter.png',
   },
 ]
