dbea1452c4f01c4d96781e941bd6616fd297bac6
wesleybridi <wesleybridi@gmail.com>
2023-05-17 19:42:21 -0300

    Criada a página "cadastre o seu estabelecimento"

---
+++ src/pages/cadastre-o-seu-estabelecimento.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function BePartner() {
+  return (
+    <>
+      <Head>
+        <title>cadastre o seu estabelecimento | Que Barbada</title>
+      </Head>
+    </>
+  )
+}
