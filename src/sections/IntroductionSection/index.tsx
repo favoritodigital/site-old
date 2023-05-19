acd792c817fe79f8ecd274e4be01c0d520c68b13
wesleybridi <wesleybridi@gmail.com>
2023-05-18 21:22:38 -0300

    Deletado comentários da section "Introduction"

--- src/sections/IntroductionSection/index.tsx
+++ src/sections/IntroductionSection/index.tsx
@@ -37,49 +37,4 @@ export default function IntroductionSection() {
       </div>
     </div>
   )
-
-  // <>
-  //   <div id='inicio' className={styles.container}>
-  //     <div className={styles.mobileContent}>
-  //       <h1 className={styles.sectionTitle}>encontre os melhores rolês em um único lugar!</h1>
-  //       <div className={styles.sectionContent}>
-  //         <div className={styles.text}>
-  //           <p>no nosso app, você tem acesso aos melhores rolês de maneira simples e prática.</p>
-  //           <p>disponível para download na App Store e Google Play:</p>
-  //         </div>
-
-  //       </div>
-
-  //     </div>
-  //     <div className={styles.webContent}>
-  //       <div className={styles.sectionContent}>
-  //         <div className={styles.leftColumn}>
-  //           <h1 className={styles.sectionTitle}>encontre os melhores rolês em um único lugar!</h1>
-  //           <div className={styles.text}>
-  //             <p>
-  //               no nosso app, você tem acesso aos melhores rolês de maneira simples e prática.
-  //             </p>
-  //             <p>disponível para download na App Store e Google Play:</p>
-  //           </div>
-  //           <div className={styles.downloadButtonsWeb}>
-  //             <DownloadButton type='appstore' />
-  //             <DownloadButton type='googleplay' />
-  //           </div>
-  //         </div>
-  //         <div className={styles.rightColumn}>
-  //           <div className={styles.img}>
-  //             <Image
-  //               loading='eager'
-  //               layout='responsive'
-  //               width={494}
-  //               height={1080}
-  //               src='/images/app/app-perspective.png'
-  //               alt='Imagem do aplicativo Que Barbada'
-  //             />
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </>
 }
