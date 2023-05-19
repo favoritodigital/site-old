0f936d56b8543effd396cf08d62569705d46245a
wesleybridi <wesleybridi@gmail.com>
2023-05-18 21:22:38 -0300

    Removida importação não utilizada

--- src/components/HowItWorksCarousel/index.tsx
+++ src/components/HowItWorksCarousel/index.tsx
@@ -5,7 +5,6 @@ import 'swiper/css'
 import 'swiper/css/effect-fade'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-import 'swiper/css/effect-coverflow'
 
 import styles from './styles.module.css'
 
