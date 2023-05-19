829dedfd5ab8d34930beff31830d53b4da353ca3
wesleybridi <wesleybridi@gmail.com>
2023-05-18 21:22:37 -0300

    Criação do botão de download

--- src/components/DownloadButton/index.tsx
+++ src/components/DownloadButton/index.tsx
@@ -1,45 +1,20 @@
-import Image from 'next/image'
+import DownloadIcon from '../../../public/images/icons/download.svg'
 
 import styles from './styles.module.css'
 
 interface Props {
-  type: 'appstore' | 'googleplay'
+  text: string
 }
 
-export default function DownloadButton({ type }: Props) {
+export default function DownloadButton({ text }: Props) {
   return (
-    <a
-      href={
-        type === 'appstore'
-          ? 'https://apps.apple.com/br/app/que-barbada/id1598991618'
-          : 'https://play.google.com/store/apps/details?id=com.quebarbada.quebarbada'
-      }
-      target='_blank'
-      rel='noreferrer'
-    >
-      {type === 'appstore' ? (
-        <div className={styles.button}>
-          <Image
-            priority
-            layout='responsive'
-            width={992}
-            height={325}
-            src='/images/brands/appstore.png'
-            alt='appstore'
-          />
+    <a href='https://www.google.com/' target='_blank' rel='noreferrer'>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <DownloadIcon />
+          <p className={styles.text}>{text}</p>
         </div>
-      ) : (
-        <div className={styles.button}>
-          <Image
-            priority
-            layout='responsive'
-            width={992}
-            height={325}
-            src='/images/brands/googleplay.png'
-            alt='googleplay'
-          />
-        </div>
-      )}
+      </div>
     </a>
   )
 }
---
+++ src/components/StoreDownloadButton/index.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+import styles from './styles.module.css'
+
+interface Props {
+  type: 'appstore' | 'googleplay'
+}
+
+export default function DownloadButton({ type }: Props) {
+  return (
+    <a
+      href={
+        type === 'appstore'
+          ? 'https://apps.apple.com/br/app/que-barbada/id1598991618'
+          : 'https://play.google.com/store/apps/details?id=com.quebarbada.quebarbada'
+      }
+      target='_blank'
+      rel='noreferrer'
+    >
+      {type === 'appstore' ? (
+        <div className={styles.button}>
+          <Image
+            priority
+            layout='responsive'
+            width={992}
+            height={325}
+            src='/images/brands/appstore.png'
+            alt='appstore'
+          />
+        </div>
+      ) : (
+        <div className={styles.button}>
+          <Image
+            priority
+            layout='responsive'
+            width={992}
+            height={325}
+            src='/images/brands/googleplay.png'
+            alt='googleplay'
+          />
+        </div>
+      )}
+    </a>
+  )
+}
