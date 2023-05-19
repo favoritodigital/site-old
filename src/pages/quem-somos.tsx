9bb61c23a78283d299db44e36b0a2c54cd79a87b
wesleybridi <wesleybridi@gmail.com>
2023-05-18 21:24:10 -0300

    Correção no nome dos arquivos da section "nossa trajetória"

--- src/pages/quem-somos.tsx
+++ src/pages/quem-somos.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 import AStartupThatCameToHelpSection from '../sections/AStartupThatCameToHelpSection'
-import OurPath from '../sections/OurPath'
+import OurPath from '../sections/OurPathSection'
 
 export default function Home() {
   return (
