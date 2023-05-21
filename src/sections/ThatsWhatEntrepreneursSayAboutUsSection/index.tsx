722d3d44945fe1540231ac953744724c01034a22
wesleybridi <wesleybridi@gmail.com>
2023-05-21 12:56:53 -0300

    Adicionado o botão para download na section "é isso que os empreendedores falam sobre nós"

--- src/sections/ThatsWhatEntrepreneursSayAboutUsSection/index.tsx
+++ src/sections/ThatsWhatEntrepreneursSayAboutUsSection/index.tsx
@@ -1,3 +1,4 @@
+import DownloadButton from '../../components/DownloadButton'
 import ThatsWhatEntrepreneursSayAboutUsCarousel from '../../components/ThatsWhatEntrepreneursSayAboutUsCarousel'
 import styles from './styles.module.css'
 
@@ -5,7 +6,10 @@ export default function ThatsWhatEntrepreneursSayAboutUsSection() {
   return (
     <div id='é-isso-que-os-empreendedores-falam-sobre-nós' className={styles.container}>
       <h1 className={styles.title}>é isso que os empreendedores falam sobre nós...</h1>
-      <ThatsWhatEntrepreneursSayAboutUsCarousel />
+      <div className={styles.carousel}>
+        <ThatsWhatEntrepreneursSayAboutUsCarousel />
+      </div>
+      <DownloadButton text='baixe agora' />
     </div>
   )
 }
