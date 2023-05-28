e862a94aab95ea8cc7e418c16f3718eec5c43961
wesleybridi <wesleybridi@gmail.com>
2023-05-28 10:21:49 -0300

    Adicionada a section "um jeito lucrativo de facilitar os rolês" para web e mobile

--- src/pages/seja-um-franqueado.tsx
+++ src/pages/seja-um-franqueado.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import YouCanProfitWithEachBusinessSection from '../sections/YouCanProfitWithEachBusinessSection'
 import HowOurBusinessWorkSection from '../sections/HowOurBusinessWorkSection'
+import AProfitableWaySection from '../sections/AProfitableWaySection'
 
 export default function BeAFranchisee() {
   return (
@@ -11,6 +12,7 @@ export default function BeAFranchisee() {
       </Head>
       <YouCanProfitWithEachBusinessSection />
       <HowOurBusinessWorkSection />
+      <AProfitableWaySection />
     </>
   )
 }
