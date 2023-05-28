4fd494bc63d848862072f36968daa3cabfc093e2
wesleybridi <wesleybridi@gmail.com>
2023-05-28 10:21:49 -0300

    Adicionado a página "seja um franqueado"

---
+++ src/pages/seja-um-franqueado.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function BeAFranchisee() {
+  return (
+    <>
+      <Head>
+        <title>seja um franquado | Que Barbada</title>
+      </Head>
+    </>
+  )
+}
