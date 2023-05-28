6b01ee742e9adc30d88b2081ddcc659f881548e2
wesleybridi <wesleybridi@gmail.com>
2023-05-28 10:15:55 -0300

    Adicionado o link correto para o formúlario da section "seja visto pelo seu cliente ideal a partir de hoje!"

--- src/sections/BeSeenByYourCustomersSection/index.tsx
+++ src/sections/BeSeenByYourCustomersSection/index.tsx
@@ -6,7 +6,7 @@ export default function BeSeenByYourCustomersSection() {
     <div className={styles.container}>
       <h1 className={styles.title}>seja visto pelo seu cliente ideal a partir de hoje!</h1>
       <div className={styles.form}>
-        <iframe src='https://app.pipefy.com/public/form/76Hk7joU?embedded=true'></iframe>
+        <iframe src='https://app.pipefy.com/public/form/TKVxxmPf?embedded=true%27'></iframe>
       </div>
       <p className={styles.paragraph}>ou se preferir, clique no botão abaixo e fale conosco</p>
       <WhatsappButton text='quero me cadastrar' />
