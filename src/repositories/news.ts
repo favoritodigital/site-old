3643ac7167ebd7fccff81262b4e06a2044240e53
wesleybridi <wesleybridi@gmail.com>
2023-05-28 22:55:06 -0300

    Atualizadas as noticías da section "Que Barbada na mídia"

--- src/repositories/news.ts
+++ src/repositories/news.ts
@@ -1,8 +1,8 @@
 export const news = [
   {
     name: 'Engeplus',
-    url: 'https://www.engeplus.com.br/noticia/espaco-empreendedor/2022/aplicativo-promete-facilitar-happy-hour-em-criciuma-e-regiao',
-    title: 'Aplicativo promete facilitar Happy Hour em Criciúma e Região',
+    url: 'https://www.engeplus.com.br/noticia/economia/2023/187392-que-barbada-comemora-um-ano-de-startup-e-1-lugar-no-premio-inova-criciuma',
+    title: 'Que Barbada comemora um ano de startup e 1º Lugar no Prêmio Inova Criciúma',
     imgFilePath: '/images/news/engeplus.png',
   },
   {
@@ -13,14 +13,15 @@ export const news = [
   },
   {
     name: 'Diário da Jaraguá',
-    url: 'https://www.diariodajaragua.com.br/geral/aplicativo-criado-em-jaragua-ajuda-clientes-a-encontrarem-bares-e/473264/',
-    title: 'Aplicativo criado em Jaraguá ajuda clientes a encontrarem bares e restaurantes ideais',
-    imgFilePath: '/images/news/diariodajaragua.png',
+    url: 'https://agoranovale.com.br/noticias/valedotaquari/startup-premiada-de-sc-chega-em-lajeado-para-garantir-o-seu-happy-hour/',
+    title: 'Startup premiada de SC chega em Lajeado para garantir o seu happy hour',
+    imgFilePath: '/images/news/agoranovale.png',
   },
   {
     name: 'Super Nova FM',
-    url: 'https://www.supernova.fm/aplicativo-ajuda-clientes-a-encontrarem-bares-e-restaurantes/',
-    title: 'Aplicativo ajuda clientes a encontrarem bares e restaurantes',
-    imgFilePath: '/images/news/supernova.png',
+    url: 'https://ndmais.com.br/economia/dupla-larga-carreira-consolidada-para-lancar-aplicativo-gastronomico-no-norte-catarinense/',
+    title:
+      'Dupla larga carreira consolidada para lançar aplicativo gastronômico no Norte Catarinense',
+    imgFilePath: '/images/news/ndmais.png',
   },
 ]
