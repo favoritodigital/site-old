f3b553001c93a7a21a5e660ec359ccc03bcf437f
wesleybridi <wesleybridi@gmail.com>
2023-06-06 21:47:59 -0300

    Inserido texto na section "seja visto pelo seu cliente ideal a partir de hoje!" da página cadastre o seu estabelecimento

--- src/sections/BeSeenByYourCustomersSection/index.tsx
+++ src/sections/BeSeenByYourCustomersSection/index.tsx
@@ -5,6 +5,9 @@ export default function BeSeenByYourCustomersSection() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>seja visto pelo seu cliente ideal a partir de hoje!</h1>
+      <p className={styles.paragraph}>
+        preencha o formulário abaixo e a nossa equipe entrará em contato com você
+      </p>
       <div className={styles.form}>
         <iframe src='https://app.pipefy.com/public/form/TKVxxmPf?embedded=true%27'></iframe>
       </div>
