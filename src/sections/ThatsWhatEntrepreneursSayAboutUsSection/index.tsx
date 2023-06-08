6300dd3578e7673455bdbed3388ac7f1a015ba62
wesleybridi <wesleybridi@gmail.com>
2023-06-08 18:02:28 -0300

    Alterada a disposição e tamanhos dos elementos, removido o carousel e inserido um vídeo da section "é isso que os empreendedores falam sobre nós" da página "cadastre o seu estabelecimento"

--- src/sections/ThatsWhatEntrepreneursSayAboutUsSection/index.tsx
+++ src/sections/ThatsWhatEntrepreneursSayAboutUsSection/index.tsx
@@ -1,13 +1,29 @@
+import YouTube, { YouTubeProps } from 'react-youtube'
+
 import DownloadButton from '../../components/DownloadButton'
-import ThatsWhatEntrepreneursSayAboutUsCarousel from '../../components/ThatsWhatEntrepreneursSayAboutUsCarousel'
 import styles from './styles.module.css'
+
+const mobileOpts: YouTubeProps['opts'] = {
+  height: '174',
+  width: '310',
+}
+
+const webOpts: YouTubeProps['opts'] = {
+  height: '430',
+  width: '765',
+}
 
 export default function ThatsWhatEntrepreneursSayAboutUsSection() {
   return (
     <div id='é-isso-que-os-empreendedores-falam-sobre-nós' className={styles.container}>
       <h1 className={styles.title}>é isso que os empreendedores falam sobre nós...</h1>
-      <div className={styles.carousel}>
-        <ThatsWhatEntrepreneursSayAboutUsCarousel />
+      <div className={styles.video}>
+        <div className={styles.mobile}>
+          <YouTube opts={mobileOpts} videoId={'T43sLq62O1s'} />
+        </div>
+        <div className={styles.web}>
+          <YouTube opts={webOpts} videoId={'T43sLq62O1s'} />
+        </div>
       </div>
       <DownloadButton text='baixe agora' />
     </div>
