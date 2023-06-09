d1620e749ee076869d148492bb83ba2fcf1fa1f2
wesleybridi <wesleybridi@gmail.com>
2023-06-09 20:56:51 -0300

    Alterado texto na section "chega de dúvidas na hora de sair" da página inicial conforme solicitação

--- src/sections/NoMoreQuestionsSection/index.tsx
+++ src/sections/NoMoreQuestionsSection/index.tsx
@@ -7,7 +7,7 @@ export default function NoMoreQuestions() {
       <h1 className={styles.title}>chega de dúvidas na hora de sair</h1>
       <Ilustration />
       <p className={styles.paragraph}>
-        no app Que Barbada você encontra as informações necessárias para escolher o seu rolê ideal,
+        no aplicativo Que Barbada você encontra as informações necessárias para escolher o seu rolê ideal,
         em apenas alguns toques
       </p>
     </div>
