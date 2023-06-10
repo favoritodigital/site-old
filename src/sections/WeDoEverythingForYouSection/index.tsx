99260f17b20abaa4b63ef3e49497eb431eda6314
wesleybridi <wesleybridi@gmail.com>
2023-06-09 23:10:55 -0300

    Refeito o layout da section "nós fazemos tudo por você" da página "cadastre o seu estabelecimento" para as versões web e mobile

--- src/sections/WeDoEverythingForYouSection/index.tsx
+++ src/sections/WeDoEverythingForYouSection/index.tsx
@@ -1,20 +1,25 @@
 import DownloadButton from '../../components/DownloadButton'
 import styles from './styles.module.css'
 
+import LightBulb from '../../../public/images/icons/light-bulb.svg'
+
 export default function WeDoEverythingForYou() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>nós fazemos tudo por você</h1>
-      <div className={styles.text}>
-        <p>além do cadastro de informações, também disponibilizamos:</p>
-        <p>
-          ✔ relatórios mensais com as suas métricas
-          <br />✔ banner dentro do app (sob consulta)
-          <br />✔ notificações personalizadas (sob consulta)
-        </p>
+      <div className={styles.icon}>
+        <LightBulb />
       </div>
-      <div className={styles.btn}>
-        <DownloadButton text={'baixe o app grátis'} />
+      <p className={styles.paragraph}>além do cadastro de informações, também disponibilizamos:</p>
+      <div className={styles.secondColumn}>
+        <div className={styles.text}>
+          <p className={styles.paragraphItem}>✔ relatórios mensais com as suas métricas</p>
+          <p className={styles.paragraphItem}>✔ banner dentro do app (sob consulta)</p>
+          <p className={styles.paragraphItem}>✔ notificações personalizadas (sob consulta)</p>
+        </div>
+        <div className={styles.btn}>
+          <DownloadButton text={'baixe o app grátis'} />
+        </div>
       </div>
     </div>
   )
