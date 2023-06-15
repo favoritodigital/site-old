4314f4672707da6919c88578e7c8021d97a01f9a
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-06-15 00:31:57 -0300

    corrigido texto

--- src/sections/UsersRecommendationsSection/index.tsx
+++ src/sections/UsersRecommendationsSection/index.tsx
@@ -8,7 +8,7 @@ export function UsersRecommendationsSection() {
     <div id='quem-usa-recomenda-section' className={styles.container}>
       <h1 className={styles.title}>quem usa, recomenda</h1>
       <p className={styles.paragraph}>
-        somo 5 estrelas na App Store e na Google Play, com <span>+ de 800 avaliações</span>!
+        somos 5 estrelas na App Store e na Google Play, com <span>+ de 850 avaliações</span>!
       </p>
       <div className={styles.carousel}>
         <UsersRecommendationCarousel />
