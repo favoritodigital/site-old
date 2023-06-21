c500101ca5536adc4493b34d8dafc555dc2e07cb
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-06-21 13:00:14 -0300

    adicionado integração com o Facebook Pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
@@ -26,6 +27,32 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property='og:url' content='https://www.quebarbada.com/' />
         <meta property='og:image' content='https://www.quebarbada.com/quebarbada.png' />
       </Head>
+      <Script
+        id='fb-pixel'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+                   !(function (f, b, e, v, n, t, s) {
+                     if (f.fbq) return
+                     n = f.fbq = function () {
+                       n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                     }
+                     if (!f._fbq) f._fbq = n
+                     n.push = n
+                     n.loaded = !0
+                     n.version = '2.0'
+                     n.queue = []
+                     t = b.createElement(e)
+                     t.async = !0
+                     t.src = v
+                     s = b.getElementsByTagName(e)[0]
+                     s.parentNode.insertBefore(t, s)
+                   })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+                   fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}')
+                   fbq('track', 'PageView')
+                  `,
+        }}
+      />
       <CookiesProvider>
         <BaseLayout>
           <>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </Head>
       <link rel='preconnect' href='https://fonts.googleapis.com' />
       <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'true'} />
       <link
