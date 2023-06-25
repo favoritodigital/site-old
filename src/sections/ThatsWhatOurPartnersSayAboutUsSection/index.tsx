5156fb7dbd3a78b18b492ed3fcfeb3ce5d7e74c1
wesleybridi <wesleybridi@gmail.com>
2023-06-25 14:33:11 -0300

    Adicionado mais um vídeo na section "é isso que os parceiros falam sobre nós..." da página "cadastre o seu estabelecimento"

--- src/sections/ThatsWhatOurPartnersSayAboutUsSection/index.tsx
+++ src/sections/ThatsWhatOurPartnersSayAboutUsSection/index.tsx
@@ -12,20 +12,30 @@ const mobileOpts: YouTubeProps['opts'] = {
 }
 
 const webOpts: YouTubeProps['opts'] = {
-  height: '430',
-  width: '765',
+  height: '253',
+  width: '450',
 }
 
 export function ThatsWhatOurPartnersSayAboutUsSection() {
   return (
     <div id='é-isso-que-os-empreendedores-falam-sobre-nós' className={styles.container}>
       <h1 className={styles.title}>é isso que os parceiros falam sobre nós...</h1>
-      <div className={styles.video}>
-        <div className={styles.mobile}>
-          <YouTube opts={mobileOpts} videoId={'T43sLq62O1s'} />
+      <div className={styles.videos}>
+        <div className={styles.video}>
+          <div className={styles.mobile}>
+            <YouTube opts={mobileOpts} videoId={'T43sLq62O1s'} />
+          </div>
+          <div className={styles.web}>
+            <YouTube opts={webOpts} videoId={'T43sLq62O1s'} />
+          </div>
         </div>
-        <div className={styles.web}>
-          <YouTube opts={webOpts} videoId={'T43sLq62O1s'} />
+        <div className={styles.video}>
+          <div className={styles.mobile}>
+            <YouTube opts={mobileOpts} videoId={'lx8qJihfXZU'} />
+          </div>
+          <div className={styles.web}>
+            <YouTube opts={webOpts} videoId={'lx8qJihfXZU'} />
+          </div>
         </div>
       </div>
       {isMobile ? (
