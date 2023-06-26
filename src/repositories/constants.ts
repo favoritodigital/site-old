4e3e456b8c05d1cc121b569b1e9aa3de9a27af41
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-06-26 14:17:16 -0300

    atualizado Rating da Store

--- src/repositories/constants.ts
+++ src/repositories/constants.ts
@@ -1,5 +1,5 @@
 export const STORE_RATING = 5
-export const STORE_RATING_COUNT = 850
+export const STORE_RATING_COUNT = 900
 export const REGION_COUNT = 5
 export const CAPITAL_REGION_COUNT = 1
 export const THOUSANDS_MONTHLY_ACCESS = 25
