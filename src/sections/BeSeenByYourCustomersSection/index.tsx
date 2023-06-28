9e899f535c1205baf101f14c10cba61733ea796c
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-06-28 20:34:37 -0300

    feat: alterado inserção do formulário do bitrix

--- src/sections/BeSeenByYourCustomersSection/index.tsx
+++ src/sections/BeSeenByYourCustomersSection/index.tsx
@@ -1,22 +1,40 @@
+import { useEffect } from 'react'
+
 import { WhatsappButton } from '../../components/WhatsappButton'
 
 import styles from './styles.module.css'
 
 export function BeSeenByYourCustomersSection() {
+  useEffect(() => {
+    const scriptId = 'static/bitrix-estabelecimento-form.js'
+
+    if (document.getElementById(scriptId) != null) {
+      return
+    }
+
+    const script = document.createElement('script')
+    script.id = scriptId
+    script.src = scriptId
+    script.setAttributeNS(null, 'data-b24-form', 'inline/14/vle4l0')
+    script.setAttributeNS(null, 'data-skip-moving', 'true')
+    script.defer = true
+
+    const scriptDiv = document.getElementById('script_div')
+
+    scriptDiv?.appendChild(script)
+
+    return () => {
+      scriptDiv?.removeChild(script)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>seja visto pelo seu cliente ideal a partir de hoje!</h1>
       <p className={styles.paragraph}>
         preencha o formulário abaixo e a nossa equipe entrará em contato com você
       </p>
-      <div className={styles.form}>
-        <script
-          src='static/bitrix-estabelecimento-form.js'
-          data-b24-form='inline/14/vle4l0'
-          data-skip-moving='true'
-          defer
-        />
-      </div>
+      <div id='script_div' className={styles.form} />
       <p className={styles.paragraph}>ou se preferir, clique no botão abaixo e fale conosco</p>
       <WhatsappButton text='quero me cadastrar' />
     </div>
--- src/sections/LetsWriteHistoryTogetherSection/index.tsx
+++ src/sections/LetsWriteHistoryTogetherSection/index.tsx
@@ -1,6 +1,31 @@
+import { useEffect } from 'react'
+
 import styles from './styles.module.css'
 
 export function LetsWriteHistoryTogetherSection() {
+  useEffect(() => {
+    const scriptId = 'static/bitrix-franqueado-form.js'
+
+    if (document.getElementById(scriptId) != null) {
+      return
+    }
+
+    const script = document.createElement('script')
+    script.id = scriptId
+    script.src = scriptId
+    script.setAttributeNS(null, 'data-b24-form', 'inline/10/esgoms')
+    script.setAttributeNS(null, 'data-skip-moving', 'true')
+    script.defer = true
+
+    const scriptDiv = document.getElementById('script_div')
+
+    scriptDiv?.appendChild(script)
+
+    return () => {
+      scriptDiv?.removeChild(script)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <div id='bora-construir-uma-historia' className={styles.pin} />
@@ -8,14 +33,7 @@ export function LetsWriteHistoryTogetherSection() {
       <p className={styles.paragraph}>
         preencha o formulário abaixo e a nossa equipe entrará em contato com você
       </p>
-      <div className={styles.form}>
-        <script
-          src='/static/bitrix-franqueado-form.js'
-          data-b24-form='inline/10/esgoms'
-          data-skip-moving='true'
-          defer
-        />
-      </div>
+      <div id='script_div' className={styles.form} />
     </div>
   )
 }
