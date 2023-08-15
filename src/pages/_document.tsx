93b6402875abfbde0134bfb3ac0a68359d5787f2
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-08-15 02:22:57 -0300

    fix: corrigido importação das fontes do Google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
         </noscript>
       </Head>
       <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'true'} />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
       <link
         href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
         rel='stylesheet'
@@ -25,6 +25,7 @@ export default function Document() {
         href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap'
         rel='stylesheet'
       />
+
       <body>
         <Main />
         <NextScript />
